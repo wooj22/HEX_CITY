9f128475db3cfb34680f8be76bece5cf2b4c1e5a
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-04-23 11:13:00 +0900

    Feat | MainMenu

---
+++ Assets/02. Scripts/SceneSwitch.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public static SceneSwitch Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SceneSwithcing(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
